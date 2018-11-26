13a1f623deea10369f5049a19de40c73ed81a1da
evelina <coffecom@yandex.ru>
2018-11-26 13:35:11 +0300

    back

--- src/GUI/App.java
+++ src/GUI/App.java
@@ -1,15 +1,10 @@
 package GUI;
 
-import BL.Managers.LogInManager;
-import BL.Managers.Session;
-import BL.Note;
-import DAL.Access;
-
 public class App {
     public static void main(String[] args){
       // Access access = new Access();
         //LogInManager.addNewUser("Alice","password");
-        Session alice = new Session("Alice","password");
+        //Session alice = new Session("Alice","password");
      /*   alice.getNoteManager().add(new Note("ecology"));
         Note note = new Note("unchanged");
         alice.getNoteManager().add(note);
